f7ff5b73965df16bc07421164d6a98a5a6e91146
Dannyle7w7 <danneyl25202002@gmail.com>
2023-10-26 18:28:19 -0600

    Hola

--- ProyectoFinal/Login.Designer.cs
+++ ProyectoFinal/Login.Designer.cs
@@ -28,126 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txbUsuario = new System.Windows.Forms.TextBox();
-            this.txbContra = new System.Windows.Forms.TextBox();
-            this.chbMostrar = new System.Windows.Forms.CheckBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.lblUsuario = new System.Windows.Forms.Label();
-            this.lblContraseña = new System.Windows.Forms.Label();
-            this.lblTitulo = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.btnLogin = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // txbUsuario
+            // btnLogin
             // 
-            this.txbUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txbUsuario.Location = new System.Drawing.Point(359, 145);
-            this.txbUsuario.Name = "txbUsuario";
-            this.txbUsuario.Size = new System.Drawing.Size(240, 44);
-            this.txbUsuario.TabIndex = 0;
-            // 
-            // txbContra
-            // 
-            this.txbContra.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txbContra.Location = new System.Drawing.Point(359, 219);
-            this.txbContra.Name = "txbContra";
-            this.txbContra.PasswordChar = '*';
-            this.txbContra.Size = new System.Drawing.Size(240, 44);
-            this.txbContra.TabIndex = 1;
-            // 
-            // chbMostrar
-            // 
-            this.chbMostrar.AutoSize = true;
-            this.chbMostrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.chbMostrar.Location = new System.Drawing.Point(605, 222);
-            this.chbMostrar.Name = "chbMostrar";
-            this.chbMostrar.Size = new System.Drawing.Size(152, 41);
-            this.chbMostrar.TabIndex = 2;
-            this.chbMostrar.Text = "Mostrar";
-            this.chbMostrar.UseVisualStyleBackColor = true;
-            this.chbMostrar.CheckedChanged += new System.EventHandler(this.chbMostrar_CheckedChanged);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(359, 303);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(167, 43);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Iniciar sesión";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // lblUsuario
-            // 
-            this.lblUsuario.AutoSize = true;
-            this.lblUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblUsuario.Location = new System.Drawing.Point(170, 145);
-            this.lblUsuario.Name = "lblUsuario";
-            this.lblUsuario.Size = new System.Drawing.Size(128, 37);
-            this.lblUsuario.TabIndex = 4;
-            this.lblUsuario.Text = "Usuario";
-            // 
-            // lblContraseña
-            // 
-            this.lblContraseña.AutoSize = true;
-            this.lblContraseña.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblContraseña.Location = new System.Drawing.Point(170, 213);
-            this.lblContraseña.Name = "lblContraseña";
-            this.lblContraseña.Size = new System.Drawing.Size(183, 37);
-            this.lblContraseña.TabIndex = 5;
-            this.lblContraseña.Text = "Contraseña";
-            // 
-            // lblTitulo
-            // 
-            this.lblTitulo.AutoSize = true;
-            this.lblTitulo.Font = new System.Drawing.Font("Maiandra GD", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitulo.Location = new System.Drawing.Point(355, 48);
-            this.lblTitulo.Name = "lblTitulo";
-            this.lblTitulo.Size = new System.Drawing.Size(116, 19);
-            this.lblTitulo.TabIndex = 6;
-            this.lblTitulo.Text = "Inicio de sesión";
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 127);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 157);
-            this.pictureBox1.TabIndex = 7;
-            this.pictureBox1.TabStop = false;
+            this.btnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(103)))), ((int)(((byte)(242)))));
+            this.btnLogin.FlatAppearance.BorderSize = 0;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Location = new System.Drawing.Point(126, 442);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(197, 45);
+            this.btnLogin.TabIndex = 0;
+            this.btnLogin.Text = "btnLogin";
+            this.btnLogin.UseVisualStyleBackColor = false;
             // 
             // FormLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.lblTitulo);
-            this.Controls.Add(this.lblContraseña);
-            this.Controls.Add(this.lblUsuario);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.chbMostrar);
-            this.Controls.Add(this.txbContra);
-            this.Controls.Add(this.txbUsuario);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(63)))), ((int)(((byte)(105)))));
+            this.ClientSize = new System.Drawing.Size(430, 680);
+            this.Controls.Add(this.btnLogin);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FormLogin";
+            this.Opacity = 0.95D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Todo Color";
-            this.Load += new System.EventHandler(this.FormLogin_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TextBox txbUsuario;
-        private System.Windows.Forms.TextBox txbContra;
-        private System.Windows.Forms.CheckBox chbMostrar;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label lblUsuario;
-        private System.Windows.Forms.Label lblContraseña;
-        private System.Windows.Forms.Label lblTitulo;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnLogin;
     }
 }
 
--- ProyectoFinal/Login.cs
+++ ProyectoFinal/Login.cs
@@ -18,34 +18,10 @@ namespace ProyectoFinal
             InitializeComponent();
         }
 
-        private void chbMostrar_CheckedChanged(object sender, EventArgs e)
-        {
-            if (chbMostrar.Checked == true)
-            {
-                txbContra.PasswordChar = Convert.ToChar(0);
-            }else if (chbMostrar.Checked == false)
-            {
-                txbContra.PasswordChar = Convert.ToChar("*");
-            }
-        }
+       
 
-        private void FormLogin_Load(object sender, EventArgs e)
-        {
+    
 
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            if (txbUsuario.Text.Length>0 && txbContra.Text.Length>0)
-            {
-                
-            }
-            else
-            {
-                //Mejorar mesagge box
-                MessageBox.Show("Lo siento, el nombre de usuario o la contraseña son incorrectos.", "Error de inicio de sesión", MessageBoxButtons.OK, MessageBoxIcon.Error);
-
-            }
-        }
+        
     }
 }
