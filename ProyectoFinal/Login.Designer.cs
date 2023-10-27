2556d3483155c15657daa3349e528db48b9efc83
Justin Salazar <a345535@uach.mx>
2023-10-26 19:24:01 -0600

    Login Prueba

--- ProyectoFinal/Login.Designer.cs
+++ ProyectoFinal/Login.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.btnLogin = new System.Windows.Forms.Button();
+            this.TitleBar = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // btnLogin
@@ -36,21 +37,33 @@
             this.btnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(103)))), ((int)(((byte)(242)))));
             this.btnLogin.FlatAppearance.BorderSize = 0;
             this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnLogin.Location = new System.Drawing.Point(38, 550);
+            this.btnLogin.Location = new System.Drawing.Point(25, 358);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(350, 50);
+            this.btnLogin.Size = new System.Drawing.Size(233, 32);
             this.btnLogin.TabIndex = 0;
             this.btnLogin.Text = "btnLogin";
             this.btnLogin.UseVisualStyleBackColor = false;
             // 
+            // TitleBar
+            // 
+            this.TitleBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(43)))), ((int)(((byte)(73)))));
+            this.TitleBar.Location = new System.Drawing.Point(-1, 0);
+            this.TitleBar.Name = "TitleBar";
+            this.TitleBar.Size = new System.Drawing.Size(288, 45);
+            this.TitleBar.TabIndex = 2;
+            this.TitleBar.Paint += new System.Windows.Forms.PaintEventHandler(this.TitleBar_Paint);
+            // 
             // FormLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(63)))), ((int)(((byte)(105)))));
-            this.ClientSize = new System.Drawing.Size(430, 680);
+            this.ClientSize = new System.Drawing.Size(287, 442);
+            this.Controls.Add(this.TitleBar);
             this.Controls.Add(this.btnLogin);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FormLogin";
             this.Opacity = 0.95D;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -62,6 +75,7 @@
         #endregion
 
         private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Panel TitleBar;
     }
 }
 
