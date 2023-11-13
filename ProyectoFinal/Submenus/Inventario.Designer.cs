664ee218b10debe78926aa36d8e1ed6c79efa4f0
Justin Salazar <a345535@uach.mx>
2023-11-13 12:27:48 -0600

    Pantallas Nuevas

--- ProyectoFinal/Submenus/Inventario.Designer.cs
+++ ProyectoFinal/Submenus/Inventario.Designer.cs
@@ -28,12 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgvDatos = new System.Windows.Forms.DataGridView();
+            this.btAgregar = new System.Windows.Forms.Button();
+            this.txtBusqueda = new System.Windows.Forms.TextBox();
+            this.tlpBusquedayAgregar = new System.Windows.Forms.TableLayoutPanel();
+            this.plTop = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).BeginInit();
+            this.tlpBusquedayAgregar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvDatos
+            // 
+            this.dgvDatos.AllowUserToAddRows = false;
+            this.dgvDatos.AllowUserToDeleteRows = false;
+            this.dgvDatos.AllowUserToResizeColumns = false;
+            this.dgvDatos.AllowUserToResizeRows = false;
+            this.dgvDatos.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvDatos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvDatos.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(214)))), ((int)(((byte)(180)))));
+            this.dgvDatos.ColumnHeadersHeight = 34;
+            this.dgvDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dgvDatos.Location = new System.Drawing.Point(11, 141);
+            this.dgvDatos.Margin = new System.Windows.Forms.Padding(2);
+            this.dgvDatos.Name = "dgvDatos";
+            this.dgvDatos.ReadOnly = true;
+            this.dgvDatos.RowHeadersWidth = 62;
+            this.dgvDatos.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dgvDatos.RowTemplate.Height = 28;
+            this.dgvDatos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvDatos.Size = new System.Drawing.Size(361, 198);
+            this.dgvDatos.TabIndex = 14;
+            // 
+            // btAgregar
+            // 
+            this.btAgregar.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btAgregar.Font = new System.Drawing.Font("Nirmala UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btAgregar.Location = new System.Drawing.Point(377, 2);
+            this.btAgregar.Margin = new System.Windows.Forms.Padding(2);
+            this.btAgregar.Name = "btAgregar";
+            this.btAgregar.Size = new System.Drawing.Size(129, 32);
+            this.btAgregar.TabIndex = 2;
+            this.btAgregar.Text = "Nuevo Cliente";
+            this.btAgregar.UseVisualStyleBackColor = true;
+            // 
+            // txtBusqueda
+            // 
+            this.txtBusqueda.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBusqueda.Font = new System.Drawing.Font("Nirmala UI", 12F);
+            this.txtBusqueda.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtBusqueda.Location = new System.Drawing.Point(2, 3);
+            this.txtBusqueda.Margin = new System.Windows.Forms.Padding(2);
+            this.txtBusqueda.Name = "txtBusqueda";
+            this.txtBusqueda.Size = new System.Drawing.Size(371, 29);
+            this.txtBusqueda.TabIndex = 1;
+            this.txtBusqueda.Text = "Búsqueda";
+            // 
+            // tlpBusquedayAgregar
+            // 
+            this.tlpBusquedayAgregar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tlpBusquedayAgregar.ColumnCount = 2;
+            this.tlpBusquedayAgregar.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpBusquedayAgregar.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 133F));
+            this.tlpBusquedayAgregar.Controls.Add(this.txtBusqueda, 0, 0);
+            this.tlpBusquedayAgregar.Controls.Add(this.btAgregar, 1, 0);
+            this.tlpBusquedayAgregar.Location = new System.Drawing.Point(-1, 92);
+            this.tlpBusquedayAgregar.Margin = new System.Windows.Forms.Padding(2);
+            this.tlpBusquedayAgregar.Name = "tlpBusquedayAgregar";
+            this.tlpBusquedayAgregar.RowCount = 1;
+            this.tlpBusquedayAgregar.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpBusquedayAgregar.Size = new System.Drawing.Size(508, 36);
+            this.tlpBusquedayAgregar.TabIndex = 16;
+            // 
+            // plTop
+            // 
+            this.plTop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(73)))), ((int)(((byte)(121)))), ((int)(((byte)(107)))));
+            this.plTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.plTop.Location = new System.Drawing.Point(0, 0);
+            this.plTop.Margin = new System.Windows.Forms.Padding(2);
+            this.plTop.Name = "plTop";
+            this.plTop.Size = new System.Drawing.Size(510, 82);
+            this.plTop.TabIndex = 15;
+            // 
+            // Inventario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(510, 366);
+            this.Controls.Add(this.dgvDatos);
+            this.Controls.Add(this.tlpBusquedayAgregar);
+            this.Controls.Add(this.plTop);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Inventario";
             this.Text = "Inventario";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).EndInit();
+            this.tlpBusquedayAgregar.ResumeLayout(false);
+            this.tlpBusquedayAgregar.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvDatos;
+        private System.Windows.Forms.Button btAgregar;
+        private System.Windows.Forms.TextBox txtBusqueda;
+        private System.Windows.Forms.TableLayoutPanel tlpBusquedayAgregar;
+        private System.Windows.Forms.Panel plTop;
     }
 }
