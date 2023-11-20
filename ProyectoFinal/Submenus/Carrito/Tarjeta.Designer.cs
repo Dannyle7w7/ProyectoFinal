20fd507da3fe53b3a25149676b2d5be1137efb43
Ramsès Gòmez <ramseschivas1@hotmail.com>
2023-11-19 19:22:57 -0600

    Mega uP

---
+++ ProyectoFinal/Submenus/Carrito/Tarjeta.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace ProyectoFinal.Submenus
+{
+    partial class Tarjeta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Tarjeta));
+            this.txtNumeroTarjeta = new System.Windows.Forms.TextBox();
+            this.txtCodigoSeguridad = new System.Windows.Forms.TextBox();
+            this.btnRealizarCompra = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Tarjet1 = new System.Windows.Forms.Label();
+            this.PbTar = new System.Windows.Forms.PictureBox();
+            this.plTop = new System.Windows.Forms.Panel();
+            this.BtnCancelarcom = new System.Windows.Forms.Button();
+            this.txtFechaVencimiento = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PbTar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtNumeroTarjeta
+            // 
+            this.txtNumeroTarjeta.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtNumeroTarjeta.Location = new System.Drawing.Point(163, 71);
+            this.txtNumeroTarjeta.Name = "txtNumeroTarjeta";
+            this.txtNumeroTarjeta.Size = new System.Drawing.Size(232, 23);
+            this.txtNumeroTarjeta.TabIndex = 0;
+            // 
+            // txtCodigoSeguridad
+            // 
+            this.txtCodigoSeguridad.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtCodigoSeguridad.Location = new System.Drawing.Point(163, 182);
+            this.txtCodigoSeguridad.Name = "txtCodigoSeguridad";
+            this.txtCodigoSeguridad.Size = new System.Drawing.Size(100, 23);
+            this.txtCodigoSeguridad.TabIndex = 3;
+            // 
+            // btnRealizarCompra
+            // 
+            this.btnRealizarCompra.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(160)))), ((int)(((byte)(214)))), ((int)(((byte)(180)))));
+            this.btnRealizarCompra.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnRealizarCompra.Location = new System.Drawing.Point(117, 281);
+            this.btnRealizarCompra.Name = "btnRealizarCompra";
+            this.btnRealizarCompra.Size = new System.Drawing.Size(121, 32);
+            this.btnRealizarCompra.TabIndex = 4;
+            this.btnRealizarCompra.Text = "Realizar compra";
+            this.btnRealizarCompra.UseVisualStyleBackColor = false;
+            this.btnRealizarCompra.Click += new System.EventHandler(this.btnRealizarCompra_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(31, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Numero de tarjeta:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(7, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Fecha de vencimiento:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label3.Location = new System.Drawing.Point(118, 188);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "CVV:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label4.Location = new System.Drawing.Point(61, 229);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Plan de pago:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Un solo pago",
+            "3 Meses sin intereses",
+            "6 Meses sin intereses",
+            "12 Meses sin intereses"});
+            this.comboBox1.Location = new System.Drawing.Point(163, 226);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(201, 24);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // Tarjet1
+            // 
+            this.Tarjet1.AutoSize = true;
+            this.Tarjet1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.Tarjet1.Location = new System.Drawing.Point(160, 41);
+            this.Tarjet1.Name = "Tarjet1";
+            this.Tarjet1.Size = new System.Drawing.Size(57, 17);
+            this.Tarjet1.TabIndex = 10;
+            this.Tarjet1.Text = "Tarjeta:";
+            // 
+            // PbTar
+            // 
+            this.PbTar.Location = new System.Drawing.Point(401, 74);
+            this.PbTar.Name = "PbTar";
+            this.PbTar.Size = new System.Drawing.Size(206, 195);
+            this.PbTar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PbTar.TabIndex = 11;
+            this.PbTar.TabStop = false;
+            // 
+            // plTop
+            // 
+            this.plTop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(73)))), ((int)(((byte)(121)))), ((int)(((byte)(107)))));
+            this.plTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.plTop.Location = new System.Drawing.Point(0, 0);
+            this.plTop.Margin = new System.Windows.Forms.Padding(2);
+            this.plTop.Name = "plTop";
+            this.plTop.Size = new System.Drawing.Size(621, 27);
+            this.plTop.TabIndex = 16;
+            // 
+            // BtnCancelarcom
+            // 
+            this.BtnCancelarcom.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.BtnCancelarcom.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.BtnCancelarcom.Location = new System.Drawing.Point(263, 281);
+            this.BtnCancelarcom.Name = "BtnCancelarcom";
+            this.BtnCancelarcom.Size = new System.Drawing.Size(126, 32);
+            this.BtnCancelarcom.TabIndex = 17;
+            this.BtnCancelarcom.Text = "Cancelar compra";
+            this.BtnCancelarcom.UseVisualStyleBackColor = false;
+            this.BtnCancelarcom.Click += new System.EventHandler(this.BtnCancelarcom_Click);
+            // 
+            // txtFechaVencimiento
+            // 
+            this.txtFechaVencimiento.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtFechaVencimiento.Location = new System.Drawing.Point(163, 142);
+            this.txtFechaVencimiento.Name = "txtFechaVencimiento";
+            this.txtFechaVencimiento.Size = new System.Drawing.Size(144, 23);
+            this.txtFechaVencimiento.TabIndex = 1;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.InitialImage")));
+            this.pictureBox2.Location = new System.Drawing.Point(163, 100);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(44, 28);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 12;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox4.ErrorImage")));
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox4.InitialImage")));
+            this.pictureBox4.Location = new System.Drawing.Point(263, 100);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(44, 28);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 14;
+            this.pictureBox4.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label6.Location = new System.Drawing.Point(101, 111);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 17);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Acepta:";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.ErrorImage")));
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.InitialImage")));
+            this.pictureBox3.Location = new System.Drawing.Point(213, 100);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(44, 28);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 18;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.ErrorImage")));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(313, 100);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(44, 28);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 19;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Tarjeta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(621, 325);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.BtnCancelarcom);
+            this.Controls.Add(this.plTop);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.PbTar);
+            this.Controls.Add(this.Tarjet1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnRealizarCompra);
+            this.Controls.Add(this.txtCodigoSeguridad);
+            this.Controls.Add(this.txtFechaVencimiento);
+            this.Controls.Add(this.txtNumeroTarjeta);
+            this.Name = "Tarjeta";
+            this.Text = "Tarjeta";
+            ((System.ComponentModel.ISupportInitialize)(this.PbTar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNumeroTarjeta;
+        private System.Windows.Forms.TextBox txtCodigoSeguridad;
+        private System.Windows.Forms.Button btnRealizarCompra;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label Tarjet1;
+        private System.Windows.Forms.PictureBox PbTar;
+        private System.Windows.Forms.Panel plTop;
+        private System.Windows.Forms.Button BtnCancelarcom;
+        private System.Windows.Forms.TextBox txtFechaVencimiento;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
